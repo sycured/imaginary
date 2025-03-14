edd90702602e98a4e9ec243d49025c64c3a366c4
sycured <60801403+sycured@users.noreply.github.com>
2025-03-13 19:46:01 -0500

    feat: include vips version in Server header

--- middleware.go
+++ middleware.go
@@ -153,7 +153,7 @@ func authorizeClient(next http.Handler, o ServerOptions) http.Handler {
 
 func defaultHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Server", fmt.Sprintf("imaginary %s (bimg %s)", Version, bimg.Version))
+		w.Header().Set("Server", fmt.Sprintf("imaginary %s (bimg %s, vips %s) ", Version, bimg.Version, bimg.VipsVersion))
 		next.ServeHTTP(w, r)
 	})
 }
